20dbfc5910a70ea65575f660765ed0cb3eccab38
Siddhant Sharma <siddhants29@gmail.com>
2025-02-17 21:05:59 +0530

    Remove unused variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -174,7 +174,7 @@ resource "azurerm_network_interface_backend_address_pool_association" "pg_lb_ass
   count                   = 3
   network_interface_id    = azurerm_network_interface.pg_nic[count.index].id
   ip_configuration_name   = "internal"
-  backend_address_pool_id = [azurerm_lb_backend_address_pool.pg_pool.id]
+  backend_address_pool_id = azurerm_lb_backend_address_pool.pg_pool.id
 }
 
 # Load Balancer Rule for PostgreSQL
